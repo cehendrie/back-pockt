4c9a30f8f17a0279e150f392ed83f0fc887b93c7
cehendrie <cehendrie@icloud.com>
2018-02-11 13:25:49 -0600

    Add exception handling for requests post
    Added exception handling for access token post requests.

--- backpockt/accesstoken.py
+++ backpockt/accesstoken.py
@@ -23,7 +23,26 @@ class AccessToken(object):
         payload = {
             'consumer_key': self.customer_key,
             'code': rt}
-        r = requests.post(url, headers=headers, data=json.dumps(payload))
+
+        try:
+            r = requests.post(url, headers=headers, data=json.dumps(payload))
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print("Http Error:", err)
+            logger.error("http error: {}".format(err))
+            raise err
+        except requests.exceptions.ConnectionError as err:
+            print("Error Connecting:", err)
+            logger.error("connection error: {}".format(err))
+            raise err
+        except requests.exceptions.Timeout as err:
+            print("Timeout Error:", err)
+            logger.error("timeout: {}".format(err))
+            raise err
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something Else", err)
+            logger.error("request error: {}".format(err))
+            raise err
 
         j = r.json()
 
--- backpockt/test/test_accesstoken.py
+++ backpockt/test/test_accesstoken.py
@@ -1,8 +1,10 @@
-import requests
 import json
 
-from backpockt.requesttoken import RequestToken
+import pytest
+import requests
+
 from backpockt.accesstoken import AccessToken
+from backpockt.requesttoken import RequestToken
 
 
 class TestAccessToken(object):
@@ -21,3 +23,63 @@ class TestAccessToken(object):
         actual = access_token.retrieve()
 
         assert 'the_access_token' == actual
+
+    def test_retrieve_httperror(self, mocker):
+        mock_request_token = mocker.Mock(RequestToken)
+        mock_request_token.retrieve.return_value = 'a_request_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError()
+
+        mocker.patch.object(requests, 'post')
+        requests.post.return_value = mock_response
+
+        access_token = AccessToken('a_customer_key', mock_request_token)
+
+        with pytest.raises(requests.exceptions.HTTPError):
+            access_token.retrieve()
+
+    def test_retrieve_connectionerror(self, mocker):
+        mock_request_token = mocker.Mock(RequestToken)
+        mock_request_token.retrieve.return_value = 'a_request_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.ConnectionError()
+
+        mocker.patch.object(requests, 'post')
+        requests.post.return_value = mock_response
+
+        access_token = AccessToken('a_customer_key', mock_request_token)
+
+        with pytest.raises(requests.exceptions.ConnectionError):
+            access_token.retrieve()
+
+    def test_retrieve_timeout(self, mocker):
+        mock_request_token = mocker.Mock(RequestToken)
+        mock_request_token.retrieve.return_value = 'a_request_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.Timeout()
+
+        mocker.patch.object(requests, 'post')
+        requests.post.return_value = mock_response
+
+        access_token = AccessToken('a_customer_key', mock_request_token)
+
+        with pytest.raises(requests.exceptions.Timeout):
+            access_token.retrieve()
+
+    def test_retrieve_requestexception(self, mocker):
+        mock_request_token = mocker.Mock(RequestToken)
+        mock_request_token.retrieve.return_value = 'a_request_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.RequestException()
+
+        mocker.patch.object(requests, 'post')
+        requests.post.return_value = mock_response
+
+        access_token = AccessToken('a_customer_key', mock_request_token)
+
+        with pytest.raises(requests.exceptions.RequestException):
+            access_token.retrieve()
