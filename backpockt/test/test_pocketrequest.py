7cc429cca4b8af999ce4d4213b70e6ee10b846d0
cehendrie <cehendrie@icloud.com>
2018-02-11 16:10:55 -0600

    Add request exception handling

--- backpockt/test/test_pocketrequest.py
+++ backpockt/test/test_pocketrequest.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 from backpockt.accesstoken import AccessToken
@@ -20,3 +21,63 @@ class TestPocketRequest(object):
         actual = pocket_request.retrieve()
 
         assert 'response_text' == actual
+
+    def test_retrieve_httperror(self, mocker):
+        mock_access_token = mocker.Mock(AccessToken)
+        mock_access_token.retrieve.return_value = 'an_access_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError()
+
+        mocker.patch.object(requests, 'get')
+        requests.get.return_value = mock_response
+
+        pocket_request = PocketRequest('a_customer_key', mock_access_token)
+
+        with pytest.raises(requests.exceptions.HTTPError):
+            pocket_request.retrieve()
+
+    def test_retrieve_connectionerror(self, mocker):
+        mock_access_token = mocker.Mock(AccessToken)
+        mock_access_token.retrieve.return_value = 'an_access_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.ConnectionError()
+
+        mocker.patch.object(requests, 'get')
+        requests.get.return_value = mock_response
+
+        pocket_request = PocketRequest('a_customer_key', mock_access_token)
+
+        with pytest.raises(requests.exceptions.ConnectionError):
+            pocket_request.retrieve()
+
+    def test_retrieve_timeout(self, mocker):
+        mock_access_token = mocker.Mock(AccessToken)
+        mock_access_token.retrieve.return_value = 'an_access_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.Timeout()
+
+        mocker.patch.object(requests, 'get')
+        requests.get.return_value = mock_response
+
+        pocket_request = PocketRequest('a_customer_key', mock_access_token)
+
+        with pytest.raises(requests.exceptions.Timeout):
+            pocket_request.retrieve()
+
+    def test_retrieve_requestexception(self, mocker):
+        mock_access_token = mocker.Mock(AccessToken)
+        mock_access_token.retrieve.return_value = 'an_access_token'
+
+        mock_response = mocker.Mock(requests.Response)
+        mock_response.raise_for_status.side_effect = requests.exceptions.RequestException()
+
+        mocker.patch.object(requests, 'get')
+        requests.get.return_value = mock_response
+
+        pocket_request = PocketRequest('a_customer_key', mock_access_token)
+
+        with pytest.raises(requests.exceptions.RequestException):
+            pocket_request.retrieve()
