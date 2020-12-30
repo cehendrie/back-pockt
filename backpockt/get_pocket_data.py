0816ecc797a3fbd6f92bb7a6bf422609e6df066b
Charles Hendrie <chendrie@Charless-MacBook-Pro.local>
2020-12-30 17:58:02 -0600

    Impl process to save article data
    Created a process to save article data to a file.

--- backpockt/get_pocket_data.py
+++ backpockt/get_pocket_data.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+import os
 
 from argparse import ArgumentParser
 
@@ -53,6 +55,13 @@ def generate_article_data(json_data):
         articles.append(json.dumps(article))
     return articles
 
+def save_article_data(articles):
+    # build path to current directory and filename
+    file = os.path.join(os.getcwd(), f"back-pockt-db-{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.txt")
+    with open(file, 'w+') as f:
+        for article in articles:
+            f.write(article + "\n")
+
 def main(consumer_key, access_token):
     data = []
     for offset in range(2):
@@ -60,6 +69,7 @@ def main(consumer_key, access_token):
         articles = generate_article_data(json_data)
         data.extend(articles)
     print(data)
+    save_article_data(data)
 
 if __name__ == '__main__':
     args = arg_parser()
