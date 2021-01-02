6526d73f30b77edcc126085ec8c442391615aa91
Charles Hendrie <cehendrie@outlook.com>
2021-01-02 12:53:22 -0600

    Impl process to retrieve latest articles
    Created a new process to retrieve the latest artices since the last run
    of the app.
    
    This process is very similar to the original process that retrieves all
    the existing articles but uses the "since" value from Pocket to only
    retrieve articles since a point in time.
    
    The new articles are added to the existing db of articles.

---
+++ backpockt/get_pocket_delta.py
@@ -0,0 +1,76 @@
+import datetime
+import json
+import os
+
+from argparse import ArgumentParser
+
+import requests
+
+
+def arg_parser():
+    argparser = ArgumentParser()
+    argparser.add_argument(
+        '-k', 
+        '--key', 
+        required=True, 
+        help='a pocket consumer key')
+    argparser.add_argument(
+        '-t', 
+        '--token', 
+        required=True,
+        help='a pocket access token')
+    argparser.add_argument(
+        '-s', 
+        '--since',
+        type=int, 
+        required=True,
+        help='unix timestamp of last retrieval data')
+    args = argparser.parse_args()
+    return args
+
+def get_pocket_data(consumer_key, access_token, since):
+    headers = {
+        'X-Accept': 'application/json',
+        'Content-Type': 'application/json; charset=UTF8'
+    }
+    payload = {
+        'consumer_key': consumer_key,
+        'access_token': access_token,
+        'sort': 'newest',
+        'since': since,
+        'detailType': 'simple'
+    }
+    r = requests.get(
+        'https://getpocket.com/v3/get', 
+        headers=headers, 
+        data=json.dumps(payload))
+    return r.json()
+
+def generate_article_data(json_data):
+    articles = []
+    for id, data in json_data["list"].items():
+        article = {
+            "id": id, 
+            "resolved_title": data["resolved_title"],
+            "resolved_url": data["resolved_url"],
+            "excerpt": data["excerpt"],
+            "time_added": data["time_added"]
+        }
+        articles.append(json.dumps(article))
+    return articles
+
+def save_article_data(articles):
+    file = os.path.join(os.getcwd(), f"back-pockt-db.txt")
+    with open(file, 'a') as f:
+        for article in articles:
+            f.write(article + "\n")
+
+def main(consumer_key, access_token, since):
+    json_data = get_pocket_data(consumer_key, access_token, since)
+    data = generate_article_data(json_data)
+    save_article_data(data)
+    print(f"store since value for next run... {json_data['since']}")
+
+if __name__ == '__main__':
+    args = arg_parser()
+    main(args.key, args.token, args.since)
